b552b8db11bd68fc0b9ae87c8de7b590412e05b7
Doaa <doaa@clweb.nl>
2022-08-22 16:03:51 +0200

    the first commit

---
+++ app/Http/Controllers/UserAuth.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAuth extends Controller
+{
+    //
+    function userLogin(Request $req)
+    {
+        $data=$req->input();
+        $req->session()->put('user', $data['user']);
+        return redirect('profile');
+
+    }
+
+    function list()
+    {
+        return view('list');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <!-- Bootstrap JS -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+
+
+    </head>
+
+    <section class="bg-light pt-5 pb-5 shadow-sm">
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col-12">
+                    <h3 class="text-uppercase border-bottom mb-4">Restaurant Utrecht </h3>
+                </div>
+            </div>
+            <div class="row">
+                <!--ADD CLASSES HERE d-flex align-items-stretch-->
+                <div class="col-lg-4 mb-3 d-flex align-items-stretch">
+                    <div class="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX-o3ntzSD98SB3pR2Z87Q8dNDAWSi507LG9EuuoqJaKiUhKbrY58a19yhfIx-0WCe3E4&usqp=CAU" class="card-img-top" alt="Card Image">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">Planning</h5>
+                            <p class="card-text mb-4">there are many events and occasions in our restaurant.</p>
+                            <a href="#" class="btn btn-primary text-white mt-auto align-self-start">more information</a>
+                        </div>
+                    </div>
+                </div>
+                <!--ADD CLASSES HERE d-flex align-items-stretch-->
+                <div class="col-lg-4 mb-3 d-flex align-items-stretch">
+                    <div class="card">
+                        <img src="https://iadsb.tmgrup.com.tr/2af227/0/0/0/0/1000/702?u=https://idsb.tmgrup.com.tr/2018/08/31/1535743406316.jpg" class="card-img-top" alt="Card Image">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">Kanaleneiland</h5>
+                            <p class="card-text mb-4">Located near the river of kanaleneiland.</p>
+                            <a href="#" class="btn btn-primary text-white mt-auto align-self-start">location</a>
+                        </div>
+                    </div>
+                </div>
+                <!--ADD CLASSES HERE d-flex align-items-stretch-->
+                <div class="col-lg-4 mb-3 d-flex align-items-stretch">
+                    <div class="card">
+                        <img src="https://img.culy.nl/images/gwxPzMGQDP8nJrW22FXA9u-1c_Y=/600x0/filters:quality(80):format(jpeg):background_color(fff)/https%3A%2F%2Fwww.culy.nl%2Fwp-content%2Fuploads%2F2017%2F04%2F2_Het_Ketelhuis_Utrecht.jpg" class="card-img-top" alt="Card Image">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">Openinghours</h5>
+                            <p class="card-text mb-4">The openinghours are flexible</p>
+                            <a href="#" class="btn btn-primary text-white mt-auto align-self-start">opening hours</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>+
+</html>
